1376cb9614b84e5720492e99fe3b88d87cf8717b
Awakee <screamm97@gmail.com>
2017-10-15 15:57:20 +0300

    Added simple logic for form.

---
+++ src/js/registration.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+  var currentStep = 0;
+  var step = $("form").children(".form-registration__step");
+  $(step[0]).show();
+  $("a.next").click(function() {
+    if(currentStep == step.length-2) {
+      $(this).hide();
+      $("form input[type=submit]").show();
+    }
+  $("a.back").show();
+  currentStep++;
+  changeStep(currentStep);
+  });
+  
+  
+  $("a.back").click(function() {
+    if(currentStep == 1) {
+      $(this).hide()
+    }
+    $("form input[type=submit]").hide();
+    $("a.next").show();
+    currentStep--;
+    changeStep(currentStep);
+  });
+  
+  function changeStep(i) {
+    $(step).hide();
+    $(step[i]).show();
+  }
+
+});
