7f7d7a88b68c1eaff99316a45528ac1bd2f7335c
Awakee <screamm97@gmail.com>
2017-10-20 20:45:41 +0300

    Added check fields

--- src/js/registration.js
+++ src/js/registration.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-  //функция переключения форм.
   var currentStep = 0;
   var step = $("form").children(".form-registration__step");
   $(step[0]).show();
@@ -29,22 +28,63 @@ $(document).ready(function(){
     $(step[i]).show();
   }
 
-
-  //функция для проверки ввода.
-
   $('#form-registration').submit(function(event){
-    var login = $("#login").val();
-    var email = $("#email").val();
-    var password = $("#password").val();
-    var name = $("#name").val();
-    var sname = $("#sname").val();
-    var age = $("#age").val();
-    var country = $("#country").val();
+    var login = $("#login").val();//
+    var email = $("#email").val();//
+    var password = $("#password").val();//
+    var name = $("#name").val();//
+    var sname = $("#sname").val();//
+    var age = $("#age").val();//
+    var country = $("#country").val();//
     var city = $("#city").val();
     var street = $("#street").val();
     var emailCheck = (/[^\s@]+@[^\s@]+\.[^\s@]+/.test(email));
     event.preventDefault;
     
+  if ((email.length < 5) || (!emailCheck)) {
+    alert("Ошибка ввода Email.");
+    return;
+  }
+
+  if ((password.length < 6) || (password == "")) {
+    alert("Ошибка ввода пароля");
+    return;
+  }
+
+  if ((login.length < 3) || (login.length == "")){
+    alert("Ошибка ввода логина");
+    return;
+  }
+ 
+  if (sname.length == "") {
+    alert("Ошибка ввода фамилии");
+    return;
+  }
+
+  if (name.length == "") {
+    alert("Ошибка ввода имени");
+    return;
+  }
+
+  if (age.length == "") {
+    alert("Ошибка ввода возраста");
+    return;
+  }
+
+  if (country.length == "") {
+    alert("Ошибка ввода страны");
+    return;
+  }
+  
+  if (city.length == "") {
+    alert("Ошибка ввода города");
+    return;
+  }
+
+  if (street.length == "") {
+    alert("Ошибка ввода улицы");
+    return;
+  }
 
     alert("Registration completed");
   });
