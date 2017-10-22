6be737a8823a9ecc03802f9459da437e0ace18d8
Awakee <screamm97@gmail.com>
2017-10-22 17:00:58 +0300

    Improve registration steps/some fix

--- src/js/registration.js
+++ src/js/registration.js
@@ -1,16 +1,51 @@
 $(document).ready(function(){
-  var currentStep = 0;
   var step = $(".form-registration").children(".form-registration__step");
-  $(step[0]).show();
-  $('.bt1').addClass('btn-warning');
-  
+
+  function currentStep(){
+    var stepReg = localStorage.getItem('regStep');
+    if ((stepReg == null ) || (stepReg == "0")){
+      $(step[0]).show();
+      $('.bt1').addClass('btn-warning');
+    }
+    if (stepReg == "1"){
+      $(step[1]).show();
+      $('.bt1').addClass('btn-success'); 
+      $('.bt1').removeClass('btn-warning');
+      $('.bt2').addClass('btn-warning');
+    }
+    if (stepReg == "2"){
+      $(step[2]).show();
+      $('.bt1').addClass('btn-success');
+      $('.bt2').addClass('btn-success'); 
+      $('.bt2').removeClass('btn-warning');
+      $('.bt3').addClass('btn-warning');
+    }
+    if (stepReg == "3"){
+      $(step[3]).show();
+      $(".stepwizard").hide();
+    }
+
+  };
+
+  currentStep();
+
+  login = $("#login").val(sessionStorage.getItem('storageLogin'));
+  email = $("#email").val(sessionStorage.getItem('storageEmail'));
+  password = $("#password").val(sessionStorage.getItem('storagePassword'));
+  name = $("#name").val(sessionStorage.getItem('storageName'));
+  sname = $("#sname").val(sessionStorage.getItem('storageSname'));
+  age = $("#age").val(sessionStorage.getItem('storageAge'));
+  country = $("#country").val(sessionStorage.getItem('storageCountry'));
+  city = $("#city").val(sessionStorage.getItem('storageCity'));
+  street = $("#street").val(sessionStorage.getItem('storageStreet'));
+
   $("#form-reg_step1").submit(function(event){
     var login = $("#login").val();
     var email = $("#email").val();
     var password = $("#password").val();
     var emailCheck = (/[^\s@]+@[^\s@]+\.[^\s@]+/.test(email));
     event.preventDefault;
-    
+
     if ((email.length < 5) || (!emailCheck)) {
       alert("Ошибка ввода Email.");
       $('.bt1').addClass('btn-danger');
@@ -28,7 +63,11 @@ $(document).ready(function(){
       $('.bt1').addClass('btn-danger');
       return;
     }
-    
+
+    login = $("#login").val(sessionStorage.setItem('storageLogin', login));
+    email = $("#email").val(sessionStorage.setItem('storageEmail', email));
+    password = $("#password").val(sessionStorage.setItem('storagePassword', password));
+ 
     $('.bt1').removeClass('btn-danger');
     $('.bt1').addClass('btn-success'); 
     $('.bt1').removeClass('btn-warning');
@@ -36,6 +75,9 @@ $(document).ready(function(){
 
     $(step[1]).show();
     $(step[0]).hide();
+
+    registrationStep = 1;
+    localStorage.setItem('regStep', registrationStep);
 
   });
 
@@ -62,14 +104,21 @@ $(document).ready(function(){
       alert("Ошибка ввода возраста");
       return;
     }
+
+    registrationStep = 2;
+    localStorage.setItem('regStep', registrationStep);
+
+    name = $("#name").val(sessionStorage.setItem('storageName', name));
+    sname = $("#sname").val(sessionStorage.setItem('storageSname', sname));
+    age = $("#age").val(sessionStorage.setItem('storageAge', age));
     
     $('.bt2').removeClass('btn-danger');
     $('.bt2').addClass('btn-success'); 
     $('.bt2').removeClass('btn-warning');
     $('.bt3').addClass('btn-warning');
+
     $(step[2]).show();
     $(step[1]).hide();
-
   });
 
   $("#form-reg_step3").submit(function(event){
@@ -96,9 +145,18 @@ $(document).ready(function(){
       return;
     }
     
+    registrationStep = 3;
+    localStorage.setItem('regStep', registrationStep);
+
+    country = $("#country").val(sessionStorage.setItem('storageCountry', country));
+    city = $("#city").val(sessionStorage.setItem('storageCity', city));
+    street = $("#street").val(sessionStorage.setItem('storageStreet', street));
+
     $('.bt3').removeClass('btn-danger');
     $('.bt3').addClass('btn-success'); 
     $('.bt3').removeClass('btn-warning');
+    $(".stepwizard").hide();
     $(step[2]).hide();
+    $(step[3]).show();
   });
 });
